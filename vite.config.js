6bb35c78cbafe74faff0b052607701f4d7926180
AkashShettyy <akashshettyy22@gmail.com>
2026-03-11 15:15:23 +0530

    fix: correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/skybrief/", // ← add this, must match your repo name exactly
+  base: "/SkyBrief/", // ← add this, must match your repo name exactly
 });
